3af67bb48ac11185266a8323fded9e505cba42dc
Pascal Jäger <pascal.jaeger@gmx.de>
2011-12-01 18:47:07 +0100

    sensing and influencing with sensinghistory
    Speichert für jeden Schritt die gemessenen Einflüsse und spuckt sie auf
    Anfrage wieder aus  (in Relation zu den gelaufenen Schritten:
    value/moves)

--- src/de/hawhamburg/mi/model/Person.java
+++ src/de/hawhamburg/mi/model/Person.java
@@ -1,8 +1,13 @@
 package de.hawhamburg.mi.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
 
 import org.apache.log4j.Logger;
+
+import com.sun.tools.javac.util.Pair;
 
 import sim.engine.SimState;
 import sim.engine.Steppable;
@@ -11,6 +16,8 @@ import sim.util.Double2D;
 import sim.util.MutableDouble2D;
 import de.hawhamburg.mi.control.MiSimulation;
 import de.hawhamburg.mi.model.common.DynamicEntity;
+import de.hawhamburg.mi.model.common.Influences;
+import de.hawhamburg.mi.model.common.Position;
 import de.hawhamburg.mi.model.common.Target;
 
 /**
@@ -32,6 +39,8 @@ public class Person extends DynamicEntity implements Steppable{
 	 */
 	private Double2D myPosition;	// current position in simulation world
 	private Logger log = Logger.getRootLogger();
+	private ArrayList<SenseObject> senseList = new ArrayList<SenseObject>();
+	private SenseHistory senseHistory = new SenseHistory();
 	
 	public Person(SimState state) {
 		super(state);
@@ -67,5 +76,61 @@ public class Person extends DynamicEntity implements Steppable{
 		miSimulation.world.setObjectLocation(this, new Double2D(nextWorldPosition));
 	}
 	
+	/**
+	 * Sense the environment. Find out what influences the agent.
+	 * 
+	 * Gets the influences and their intensities at the current position and stores them
+	 * for further analysis.
+	 */
+	private void sense(){
+		// ArrayList<Influence> influences = miSimulation.mapLayer.getInfluences(myPosition);
+		
+	}
 	
+	/**
+	 * 
+	 * @author Pascal
+	 *
+	 * Currently local, we'll see where it gets us to.
+	 */
+	private class SenseObject{
+		private Position pos;
+		
+		
+	}
+
+	private class SenseHistory{
+		int moves; // the number of moves the agent has made
+		HashMap<Influences, Integer> sensedInfluences = 
+										new HashMap<Influences, Integer>();
+		
+		public void addSensation(SenseObject sensobj){
+			// zerlege das senseobject und adde seine Teile in die sensedInfluences
+			addInfluence(null);
+		}
+		
+		private void addInfluence(Pair<Influences, Integer> influence){
+			int intensity = sensedInfluences.get(influence.fst);
+			sensedInfluences.put(influence.fst, intensity+influence.snd);
+		}
+		
+		public HashMap<Influences, Integer> calculateHistory(){
+			// deep copying the hashmap
+			HashMap<Influences, Integer>  copy = new HashMap<Influences, Integer>();
+			Iterator<Entry<Influences, Integer>> it = sensedInfluences.entrySet().iterator();
+			while(it.hasNext()){
+				copy.put(it.next().getKey(), it.next().getValue());
+			}
+			
+			// level values down with number of moves made
+			Iterator<Entry<Influences, Integer>> it2 = copy.entrySet().iterator();
+			while(it2.hasNext()){
+				Entry<Influences, Integer> influ = it2.next();
+				copy.put(influ.getKey(), influ.getValue()/moves);
+			}
+			
+			return copy;
+		}
+		
+	}
 }
