57e45696f97abe2318d04916ea1f7451a1a327e5
Jan Meier <jan@codejunky.org>
2011-12-08 17:04:39 +0100

    Pathfinder Klasse hinzugefügt

---
+++ src/de/hawhamburg/mi/control/Pathfinder.java
@@ -0,0 +1,11 @@
+package de.hawhamburg.mi.control;
+
+import java.util.ArrayList;
+
+import de.hawhamburg.mi.model.common.Position;
+
+public class Pathfinder {
+	public ArrayList<Position> getPath(Position from, Position to) {
+		return new ArrayList<Position>();
+	}
+}
